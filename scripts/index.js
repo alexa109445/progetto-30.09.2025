6e13fb504c4a17801caf1c98e0831e126e026a94
alexa109445 <alemanolache109@gmail.com>
2025-09-30 16:49:28 +0200

    JS

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const myCat = {
+  name: "Leo",
+  age: 4,
+  broad: "Europeo",
+  scratches: true,
+};
+console.log("oggetto intero", myCat);
+let n1 = 6;
+let n2 = 7;
+console.log("numeropiùgrande", n2);
+
+n3 = 6;
+if (n3 !== 5) {
+  console.log("not equal");
+}
+
+n4 = 25 % 5;
+
+console.log("divisibile Per 5", n4);
+
+let n5 = 8;
+let n6 = 5;
+if (n5 + n6 || n5 - n6 === 8) {
+  console.log("non corretto");
+}
+else if (n5=8){console.log("corretto")}
+
+let totalShoppingCart=55
+if(totalShoppingCart<50){console.log("Spedizione 10euro")}
+else if(totalShoppingCart>50){console.log("Spedizione gratuita")}
+
+
+const prezzi=[20, 20, 15]
+const prezzi scontati=[16, 16, 12]
+let totalShoppingCart=44
+console.log("totale scontato", totalShoppingCart )
+if(totalShoppingCart>50){console.log("Spedizione 10euro")}
+else if(totalShoppingCart<50){console.log("Spedizione gratuita")}
+
